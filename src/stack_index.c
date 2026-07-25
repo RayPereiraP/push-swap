3146c250886c3111dac5ba65abbb5237aaad48bb
wesleygf <wesleygfsilva@hotmail.com>
2026-07-24 23:50:40 -0300

    feature: binary search implemented

--- src/stack_index.c
+++ src/stack_index.c
@@ -11,6 +11,32 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static int binary_search(int *list, int value, int size)
+{
+    int start;
+    int end;
+    int mid;
+    
+    start = 0;
+    end = size - 1;
+    while (start <= end)
+    {
+        mid = (start + end) / 2;
+        if (value < list[mid])
+            end = mid - 1;
+        else if (value > list[mid])
+            start = mid + 1;
+        else
+            return (mid);
+    }
+    return (-1);
+}
+
+static int *quicksort(int *list, int size)
+{
+    
+}
 void stack_index(int *stack)
 {
     int	i;
@@ -18,7 +44,3 @@ void stack_index(int *stack)
 }
 
 
-static int *quicksort(int *list)
-{
-    
-}
