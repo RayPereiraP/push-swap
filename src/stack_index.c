856aedf02fdd6fe564086f503f1b3dd6c098a69c
wesleygf <wesleygfsilva@hotmail.com>
2026-07-26 21:55:33 -0300

    fix: changed from merge sort with malloc to only with on aux array

--- src/stack_index.c
+++ src/stack_index.c
@@ -12,122 +12,83 @@
 
 #include "push_swap.h"
 
-static int binary_search(int *list, int value, int size)
+static int	binary_search(int *list, int value, int size)
 {
-    int start;
-    int end;
-    int mid;
-    
-    start = 0;
-    end = size - 1;
-    while (start <= end)
-    {
-        mid = (start + end) / 2;
-        if (value < list[mid])
-            end = mid - 1;
-        else if (value > list[mid])
-            start = mid + 1;
-        else
-            return (mid);
-    }
-    return (-1);
+	int	start;
+	int	end;
+	int	mid;
+
+	start = 0;
+	end = size - 1;
+	while (start <= end)
+	{
+		mid = (start + end) / 2;
+		if (value < list[mid])
+			end = mid - 1;
+		else if (value > list[mid])
+			start = mid + 1;
+		else
+			return (mid);
+	}
+	return (-1);
 }
 
-static int *merge(int *left, int *right, int left_size, int *array, int size)
+static int	*merge(int *array, int *sorted, int start, int middle, int finish)
 {
-    int i;
-    int j;
-    int k;
+	int	i;
+	int	j;
+	int	k;
 
-    if (!left || !right)
-        return (NULL);
-    i = 0;
-    j = 0;
-    k = 0;
-    while (i < left_size && j < size - left_size)
-    {
-        if (left[i] < right[j])
-            array[k++] = left[i++];
-        else
-            array[k++] = right[j++];
-    }
-    while (i < left_size)
-        array[k++] = left[i++];
-    while (j < size - left_size)
-        array[k++] = right[j++];
-    return (array);
+	i = start;
+	j = middle + 1;
+	k = start;
+	while (i < middle && j < finish - middle)
+	{
+		if (array[i] < array[j])
+			sorted[k++] = array[i++];
+		else
+            sorted[k++] = array[j++];
+	}
+	return (array);
 }
 
-static int *new_array(int *array, int start, int finish)
+static int	*merge_sort(int *array, int *sorted, int start, int finish)
 {
-    int *tmp;
-    int i;
+	int	middle;
 
-    tmp = malloc(sizeof(int) * (finish - start));
-    if (!tmp)
-        return (NULL);
-    i = 0;
-    while (start < finish)
-    {
-        tmp[i] = array[start];
-        start++;
-        i++;
-    }
-    return (tmp);
+	if (start < finish)
+	{
+		middle = start + (finish - start) / 2;
+		merge_sort(array, sorted, start, middle);
+		merge_sort(array, sorted, middle + 1, finish);
+		merge(array, sorted, start, middle, finish);
+	}
+	else
+		return (array);
 }
 
-static int *merge_sort(int *array, int size)
+int	*index_array(int *stack, int size)
 {
-    int *left;
-    int *right;
+	int	*idx_array;
+	int	*sorted_array;
+	int	i;
 
-    if (size > 1)
-    {
-        left = new_array(array, 0, size/2);
-        right = new_array(array, size/2, size);
-        if (!right || !left)
-        {
-            free(right);
-            free(left);
-            return (NULL);
-        }
-        left = merge_sort(left, size/2);
-        right = merge_sort(right, (size - (size / 2)));
-        array = merge(left, right, size/2, array, size);
-        free(left);
-        free(right);
-        if (!array)
-            return (NULL);
-    }
-    if (size == 1)
-        return (array);
-    return (array);
-}
-
-int  *index_array(int *stack, int size)
-{
-    int	*idx_array;
-    int	*sorted_array;
-    int	i;
-
-    sorted_array = new_array(stack, 0, size);
-    if (!sorted_array)
-        return (NULL);
-    sorted_array = merge_sort(sorted_array, size);
-    if (!sorted_array)
-        return (NULL);
-    idx_array = malloc(sizeof(int) * size);
-    if (!idx_array)
-    {
-        free(sorted_array);
-        return (NULL);
-    }
-    i = 0;
-    while (i < size)
-    {
-        idx_array[i] = binary_search(sorted_array, stack[i], size);
-        i++;
-    }
-    free(sorted_array);
-    return (idx_array);
+	sorted_array = malloc(sizeof(int) * size);
+	if (!sorted_array)
+		return (NULL);
+	merge_sort(stack, sorted_array, 0, size);
+	idx_array = malloc(sizeof(int) * size);
+	if (!idx_array)
+	{
+		free(sorted_array);
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		idx_array[i] = binary_search(sorted_array, stack[i], size);
+		i++;
+	}
+	free(sorted_array);
+	return (idx_array);
 }
