61bf4b363e82b378d362cc78dffed9d37ca7e6aa
Wesley Golberto Ferreira Silva <wgolbert@c1r2p1.42sp.org.br>
2026-08-08 20:20:38 -0300

    fix: ft_printf only prints on the error exit

--- src/benchmark.c
+++ src/benchmark.c
@@ -6,7 +6,7 @@
 /*   By: wgolbert <wgolbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/17 15:00:04 by rayperei          #+#    #+#             */
-/*   Updated: 2026/08/08 20:11:43 by wgolbert         ###   ########.fr       */
+/*   Updated: 2026/08/08 20:19:53 by wgolbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,12 @@ static char	*get_strategy_info(int algo_flag)
 
 static void	print_ops_breakdown(t_benchmark *b)
 {
-	ft_printf(stderr, "[bench] sa: %d | sb: %d | ss: %d\n",
+	ft_printf("[bench] sa: %d | sb: %d | ss: %d\n",
 		b->sa, b->sb, b->ss);
-	ft_printf(stderr, "[bench] pa: %d | pb: %d\n", b->pa, b->pb);
-	ft_printf(stderr, "[bench] ra: %d | rb: %d | rr: %d\n",
+	ft_printf("[bench] pa: %d | pb: %d\n", b->pa, b->pb);
+	ft_printf("[bench] ra: %d | rb: %d | rr: %d\n",
 		b->ra, b->rb, b->rr);
-	ft_printf(stderr, "[bench] rra: %d | rrb: %d | rrr: %d\n",
+	ft_printf("[bench] rra: %d | rrb: %d | rrr: %d\n",
 		b->rra, b->rrb, b->rrr);
 }
 
@@ -46,8 +46,8 @@ void	print_benchmark(t_push_swap *ps, double initial_disorder, t_flags flags)
 	int	total_ops;
 
 	total_ops = get_total_ops(&ps->benchmark);
-	ft_printf(stderr, "[bench] disorder: %f%%\n", initial_disorder * 100);
-	ft_printf(stderr, "[bench] strategy: %s\n", get_strategy_info(flags.flag));
-	ft_printf(stderr, "[bench] total_ops: %d\n", total_ops);
+	ft_printf("[bench] disorder: %f%%\n", initial_disorder * 100);
+	ft_printf("[bench] strategy: %s\n", get_strategy_info(flags.flag));
+	ft_printf("[bench] total_ops: %d\n", total_ops);
 	print_ops_breakdown(&ps->benchmark);
 }
